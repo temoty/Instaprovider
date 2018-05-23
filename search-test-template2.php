b061e29294dbe4a8692c0a493dc70c076238bf78
temoty <temoty@hotmail.com>
2018-05-23 14:52:44 -0400

    modified search results template to show results heading more accurately.

--- search-test-template2.php
+++ search-test-template2.php
@@ -6,43 +6,73 @@ get_header();
 $search = new WP_Advanced_Search('my-form'); 
 ?>
 
-<div class="container">
-    <div class="row">
-        <div id="search-form" class="col-md-10 col-md-offset-1">
+    <div class="container">
+        <div class="row">
+            <div id="search-form" class="col-md-10 col-md-offset-1">
 
-            <?php
+                <?php
             $temp = $wp_query;
             $wp_query = $search->query();
             ?>
 
-            <?php $search->the_form(); ?>
+                    <?php $search->the_form(); ?>
 
-        </div>
-        
+            </div>
 
-        <div class="col-md-12">
-            <h4 class="results-count">
-                Displaying <?php echo $search->results_range( $args = array('marker' => '-') ); ?> of <?php echo $wp_query->found_posts; ?> results
-            </h4>
-        </div>
 
-        <?php
+            <div class="col-md-12">
+                <h4 class="results-count">
+
+
+
+                    <?php
+                    if(isset($_GET["tax_category"])){
+    $specialtyy = $_GET["tax_category"];
+    if ($specialtyy === "music-teachers") {
+        $specialtyy = "music teachers";
+    } elseif ($specialtyy === "life-coaches") {
+        $specialtyy = "life coaches";
+    } elseif ($specialtyy === "personal-trainers") {
+        $specialtyy = "personal trainers";
+    } elseif ($specialtyy === "artists") {
+        $specialtyy = "artists";
+    } elseif ($specialtyy === "massage") {
+        $specialtyy = "massage therapist";
+    }
+} 
+
+?>
+
+                        <!-- Displaying <?php echo $search->results_range( $args = array('marker' => '-') ); ?> of <?php echo $wp_query->found_posts; ?> results in Boston Area -->
+                        Displaying
+                        <?php echo $wp_query->found_posts; str_repeat('&nbsp;', 3); ?>
+                        <?php echo $specialtyy; ?> results in Boston Area
+
+                </h4>
+            </div>
+
+            <?php
         if (have_posts()) :
 
             while (have_posts()) : the_post(); ?>
                 <?php $post_type = get_post_type_object($post->post_type); ?>
 
                 <div class="col-md-12 provider-result">
-		    <div class="col-sm-5 provider-result-video">
-			<iframe src="https://player.vimeo.com/video/<?php the_field( 'vimeo_id' ); ?>?title=0&byline=0&portrait=0&badge=0" width="330" height="186" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-		    </div>
-		    <div class="col-sm-7 provider-result-excerpt">
-                    <article>
-                        <h4><a href="<?php the_permalink(); ?>"><?php/* the_title(); */?></a></h4>
+                    <div class="col-sm-5 provider-result-video">
+                        <iframe src="https://player.vimeo.com/video/<?php the_field( 'vimeo_id' ); ?>?title=0&byline=0&portrait=0&badge=0" width="330"
+                            height="186" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    </div>
+                    <div class="col-sm-7 provider-result-excerpt">
+                        <article>
+                            <h4>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php/* the_title(); */?>
+                                </a>
+                            </h4>
 
-                        <?php the_excerpt(); ?>
-                    </article>
-		    </div>
+                            <?php the_excerpt(); ?>
+                        </article>
+                    </div>
 
                 </div>
 
@@ -60,8 +90,7 @@ $search = new WP_Advanced_Search('my-form');
 
                 ?>
 
+        </div>
     </div>
-</div>
 
-<?php get_footer(); ?>
-
+    <?php get_footer(); ?>
