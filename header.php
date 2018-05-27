d81c1ef3bcaa78ec61adf365cec8771beec00576
temoty <temoty@hotmail.com>
2018-05-26 22:33:29 -0400

    reinstalled plugins and added tag manager code and analytics code to header.php

--- header.php
+++ header.php
@@ -11,6 +11,13 @@
   gtag('config', 'UA-772269-5');
 </script>
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T235XZ7');</script>
+<!-- End Google Tag Manager -->
 
 
 
@@ -52,6 +59,10 @@ FLTheme::head();
 </head>
 
 <body <?php body_class(); ?> itemscope="itemscope" itemtype="https://schema.org/WebPage">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T235XZ7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php
 
 FLTheme::header_code();
